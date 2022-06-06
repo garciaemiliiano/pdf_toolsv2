32928d3ff8b4cb0b1d513dbdb3d4c6e85365911c
Emiliano Garcia <garciacristianemiliano@gmail.com>
2022-06-06 18:57:03 -0300

    Arreglos menores

--- app/fn.py
+++ app/fn.py
@@ -22,15 +22,17 @@ async def reset_eof_of_pdf_return_stream(pdf_stream_in: list):
             if b"%%EOF" in x:
                 actual_line = len(pdf_stream_in) - i
                 logging.warning(">>> EOF found...")
-                break
-        return pdf_stream_in[:actual_line]
+                logging.warning(">>> EOF fixed...")
+                return pdf_stream_in[:actual_line]
+        logging.warning(">>> EOF not found...")
     except BaseException as exception:
         logging.error("Exception occurred", exc_info=True)
+        raise exception
 
 
 async def download_file(file: UploadFile, root_path: str, filename: str):
     try:
-        new_file_name = format_file_name(filename)
+        new_file_name = await format_file_name(filename)
         async with aiofiles.open(
             os.path.join(root_path, new_file_name), "wb"
         ) as out_file:
@@ -42,7 +44,7 @@ async def download_file(file: UploadFile, root_path: str, filename: str):
         raise exception
 
 
-def format_file_name(string: str):
+async def format_file_name(string: str):
     filename = (unidecode.unidecode(string)).replace(" ", "_")
     return filename
 
@@ -72,10 +74,50 @@ async def fix_EOF(files: list, root_path: str):
         for f in files:
             async with aiofiles.open(os.path.join(root_path, f), "rb") as p:
                 txt = await p.readlines()
+                p.close()
             txtx = await reset_eof_of_pdf_return_stream(txt)
             async with aiofiles.open(os.path.join(root_path, f), "wb") as f:
                 await f.writelines(txtx)
+                f.close()
         return files
+    except BaseException as exception:
+        logging.error("Exception occurred", exc_info=True)
+        raise exception
+
+
+async def merge_files(files: list(), root_path: str):
+    """Merge files async."""
+    try:
+        # Inicio del merge
+        logging.info(">>> Realizando merge...")
+        merger = PdfFileMerger(strict=False)
+        for pdf in files:
+            merger.append(os.path.join(root_path, pdf))
+        random_uuid = str(uuid.uuid4())
+        output_merge_path = f"{os.path.join(root_path, random_uuid)}_result-merge.pdf"
+        merger.write(output_merge_path)
+        merger.close()
+        return output_merge_path
+    except BaseException as exception:
+        logging.error("Exception occurred", exc_info=True)
+        raise exception
+
+
+async def download_files(files: list[UploadFile], root_path: str):
+    try:
+        arr_files = []
+        for file in files:
+            # arr_files.append(await download_file(file, root_path, file.filename))
+            filename = file.filename
+            new_file_name = await format_file_name(filename)
+            async with aiofiles.open(
+                os.path.join(root_path, new_file_name), "wb"
+            ) as out_file:
+                content = await file.read()
+                await out_file.write(content)
+                out_file.close()
+            arr_files.append(new_file_name)
+        return arr_files
     except BaseException as exception:
         logging.error("Exception occurred", exc_info=True)
         raise exception
@@ -84,19 +126,9 @@ async def fix_EOF(files: list, root_path: str):
 async def _merge(files: list[UploadFile], root_path: str):
     try:
         logging.info(">>> Descargando pdfs...")
-        arr_files = []
-        for file in files:
-            arr_files.append(await download_file(file, root_path, file.filename))
+        arr_files = await download_files(files, root_path)
         await fix_EOF(arr_files, root_path)
-        merger = PdfFileMerger(strict=False)
-        # Inicio del merge
-        logging.info(">>> Realizando merge...")
-        for pdf in arr_files:
-            merger.append(os.path.join(root_path, pdf))
-        random_uuid = str(uuid.uuid4())
-        output_merge_path = f"{os.path.join(root_path, random_uuid)}_result-merge.pdf"
-        merger.write(output_merge_path)
-        merger.close()
+        output_merge_path = await merge_files(arr_files, root_path)
         logging.info(">>> Successful merge.")
         return FileResponse(output_merge_path)
     except BaseException as exception:
