da92092227b8eda2145720ddb0eb754d8d8a225d
Emiliano Garcia <garciacristianemiliano@gmail.com>
2022-06-07 14:08:16 -0300

    Add asyncio tasks

--- app/fn.py
+++ app/fn.py
@@ -1,9 +1,10 @@
-import sys, os, os.path, PyPDF2, pdfreader, uuid, coloredlogs, logging, unidecode, shutil, aiofiles
+import sys, os, os.path, PyPDF2, pdfreader, uuid, coloredlogs, logging, unidecode, shutil, aiofiles, aiofile, asyncio
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 from PyPDF2 import PdfFileMerger, PdfFileReader
 from pdfreader import PDFDocument
 from classes import _Error
+from aiofile import AIOFile, LineReader, Writer
 
 coloredlogs.install()
 
@@ -21,8 +22,8 @@ async def reset_eof_of_pdf_return_stream(pdf_stream_in: list):
         for i, x in enumerate(pdf_stream_in[::-1]):
             if b"%%EOF" in x:
                 actual_line = len(pdf_stream_in) - i
-                logging.warning(">>> EOF found...")
-                logging.warning(">>> EOF fixed...")
+                # logging.warning(">>> EOF found...")
+                logging.warning(">>> EOF fixed")
                 return pdf_stream_in[:actual_line]
         logging.warning(">>> EOF not found...")
     except BaseException as exception:
@@ -75,6 +76,7 @@ async def fix_EOF(files: list, root_path: str):
             async with aiofiles.open(os.path.join(root_path, f), "rb") as p:
                 txt = await p.readlines()
                 p.close()
+            task_reset_eof = asyncio.create_task(reset_eof_of_pdf_return_stream(txt))
             txtx = await reset_eof_of_pdf_return_stream(txt)
             async with aiofiles.open(os.path.join(root_path, f), "wb") as f:
                 await f.writelines(txtx)
@@ -85,6 +87,15 @@ async def fix_EOF(files: list, root_path: str):
         raise exception
 
 
+async def merger_append(root_path, pdf, merger):
+    return merger.append(os.path.join(root_path, pdf))
+
+
+async def merger_write(merger, output_merge_path):
+    merger.write(output_merge_path)
+    return merger.close()
+
+
 async def merge_files(files: list(), root_path: str):
     """Merge files async."""
     try:
@@ -92,31 +103,36 @@ async def merge_files(files: list(), root_path: str):
         logging.info(">>> Realizando merge...")
         merger = PdfFileMerger(strict=False)
         for pdf in files:
-            merger.append(os.path.join(root_path, pdf))
+            task_merger = asyncio.create_task(merger_append(root_path, pdf, merger))
+            await task_merger
         random_uuid = str(uuid.uuid4())
         output_merge_path = f"{os.path.join(root_path, random_uuid)}_result-merge.pdf"
-        merger.write(output_merge_path)
-        merger.close()
+        task_merger_write = asyncio.create_task(merger_write(merger, output_merge_path))
+        await task_merger_write
+        logging.info(">>> Successful merge ✓")
         return output_merge_path
     except BaseException as exception:
-        logging.error("Exception occurred", exc_info=True)
+        logging.error("ErrorMerge occurred", exc_info=True)
         raise exception
 
 
 async def download_files(files: list[UploadFile], root_path: str):
     try:
+        logging.info(">>> Descargando pdfs...")
         arr_files = []
         for file in files:
-            # arr_files.append(await download_file(file, root_path, file.filename))
             filename = file.filename
-            new_file_name = await format_file_name(filename)
-            async with aiofiles.open(
+            # logging.info(">>> CHANGE FILENAME ACTUAL...")
+            task_new_file_name = asyncio.create_task(format_file_name(filename))
+            new_file_name = await task_new_file_name
+            # logging.info(">>> FINISH FILENAME...")
+            async with aiofile.async_open(
                 os.path.join(root_path, new_file_name), "wb"
             ) as out_file:
                 content = await file.read()
                 await out_file.write(content)
-                out_file.close()
             arr_files.append(new_file_name)
+        logging.info(">>> Downloads finish ✓")
         return arr_files
     except BaseException as exception:
         logging.error("Exception occurred", exc_info=True)
@@ -125,11 +141,13 @@ async def download_files(files: list[UploadFile], root_path: str):
 
 async def _merge(files: list[UploadFile], root_path: str):
     try:
-        logging.info(">>> Descargando pdfs...")
-        arr_files = await download_files(files, root_path)
-        await fix_EOF(arr_files, root_path)
-        output_merge_path = await merge_files(arr_files, root_path)
-        logging.info(">>> Successful merge.")
+        task_download_files = asyncio.create_task(download_files(files, root_path))
+        arr_files = await task_download_files
+        task_fix_eof = asyncio.create_task(fix_EOF(arr_files, root_path))
+        await task_fix_eof
+        task_merge = asyncio.create_task(merge_files(arr_files, root_path))
+        output_merge_path = await task_merge
+        logging.info(">>> RETURNING...")
         return FileResponse(output_merge_path)
     except BaseException as exception:
         logging.error("Exception occurred", exc_info=True)
